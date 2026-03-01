47f2edf66e2e924a2e5cbe55e0db7923fde8cf73
harley <harley.reyes@cit.edu>
2026-03-01 14:22:17 +0800

    added teacher_classroom in router.py

--- apps/api/app/api/router.py
+++ apps/api/app/api/router.py
@@ -1,5 +1,5 @@
 from fastapi import APIRouter
-from app.api.routes import health, dbdebug, auth, classroom, submission
+from app.api.routes import health, dbdebug, auth, classroom, submission, teacher_classroom
 
 api_router = APIRouter()
 api_router.include_router(health.router, tags=["health"])
@@ -7,3 +7,4 @@ api_router.include_router(dbdebug.router, tags=["router"])
 api_router.include_router(auth.router, tags=["auth"])
 api_router.include_router(classroom.router, tags=["classroom"])
 api_router.include_router(submission.router, tags=["submission"])
+api_router.include_router(teacher_classroom.router, tags=["teacher_classroom"])
